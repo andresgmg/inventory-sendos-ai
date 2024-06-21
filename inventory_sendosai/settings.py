1b64f5135df162a92c0ca55563f10b218fd37443
Andres Marquez <72410509+andresgmg@users.noreply.github.com>
2024-06-21 13:28:01 -0400

    secret_key (for educational pruposes)

--- inventory_sendosai/settings.py
+++ inventory_sendosai/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "django-insecure-25*f!nkvy0n_9&dn8i#mgi((#ruj5=auef_3iq39&4ik&4cqzx"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
